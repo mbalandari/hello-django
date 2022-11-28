ffa399fab0e07effc62ce77f2153d328c7313e68
codingMaz <m.balandari.toroghi@gmail.com>
2022-11-29 02:13:32 +0330

    add templates and homepage

--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView
 
-urlpatterns = [path("", homePageView, name="home")]
+urlpatterns = [path("", HomePageView.as_view(), name="home")]
