c24daf9a8254d1015bbf71f7f3b610c9a5b61a26
codingMaz <m.balandari.toroghi@gmail.com>
2022-11-29 13:24:25 +0330

    Heroku deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-bg@^!h1*r@ce9spj8294q)%ibj)@gjcqp=-i%4+w@9@c*e^)h=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
